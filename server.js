078519eb38a1cc0a7a71180dcfafb268433fab46
kibarke <kibarkework@gmail.com>
2024-04-03 15:20:54 -0400

    I started fixing the server file, need to switch it to js branch tho

--- server.js
+++ server.js
@@ -1,17 +1,64 @@
 var fs = require("fs");
-const inquirer = ("inquirer");
+var inquirer = ("inquirer");
 const path = require("path");
+const dragonBird = require('./db/connection');
+const { error } = require("console");
 
-const questions = [
-    {
-        type: "checkbox",
-        message: "What would you like to do?",
-        choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
-    },
-    {
-        type: "input",
-        message: "What is the name of the department?",
-    },
+// establish a connection
+dragonBird.connect(err => {
+    if (err) throw error;
+    console.log('Hey! The database is connected!');
+    people_tracker();
+});
+
+
+var questions = function () {
+    inquirer.prompt([{
+        // Command line questions
+            type: "checkbox",
+            name: "prompt",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+    }]).then((answers) => {
+        if (answers.prompt === 'View All Department') {
+            dragonBird.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Departments: ");
+                console.table(result);
+                people_tracker();
+        });
+    } else if (answers.prompt === 'View All Roles') {
+        dragonBird.query(`SELECT * FROM role`, (err, result) => {
+            if (err) throw err;
+            console.log("Viewing All Roles: ");
+            console.table(result);
+            people_tracker();
+        });
+    } else if (answers.prompt === 'Add A Department') {
+        inquirer.prompt([{
+            // To add a department
+            type: "input",
+            name: "department",
+            message: "What is the name of the department?",
+            validate: departmentsInput => {
+                if (departmentsInput) {
+                    return true;
+                } else {
+                    console.log("Whoops! We forgot to add the department!");
+                    return false;
+                }
+            }
+        }]);
+    } else if (answers.prompt === 'View All Employees') {
+        dragonBird.query(`SELECT * FROM role`, (err, result) => {
+            if (err) throw err;
+            console.log("Viewing All Roles: ");
+            console.table(result);
+            people_tracker();
+        });
+    } 
+ [
+    
     {
         type: "input",
         message: "What is the name of the role?",
@@ -40,6 +87,6 @@ const questions = [
     {
         type: "checkbox",
         message: "Who is the employee's manger?",
-        choices: ["None", "John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Kunal Singh", "Malia Brown"],
+        choices: ["None", "Chiron", "Albus Dumbledore", "Tony Stark", "Sean Maguire", "Gandalf The Gray", "Charles Xavier"],
     },
 ];
