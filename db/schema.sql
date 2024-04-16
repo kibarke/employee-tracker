13fd034822b0b34a884010a312db18218b471ec0
kibarke <kibarkework@gmail.com>
2024-04-16 12:31:34 -0400

    Im trying to fix my error through adding the seeds to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS people_tracker_db;
+CREATE DATABASE people_tracker_db;
+
+USE people_tracker_db;
+
 -- department table, keeps tracks of the names --
 
 CREATE TABLE departments (
@@ -23,3 +28,35 @@ CREATE TABLE employees (
     role_id INTEGER,
     manager_id INTEGER
 );
+
+-- the hard part, making an actual table
+
+-- Department table
+
+INSERT INTO departments (name),
+VALUES
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales'),
+    ('Service');
+
+
+-- Roles table
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Engineer', 100000, 1),
+  ('Financial Analyst', 75000, 2),
+  ('Lawyer', 200000, 3),
+  ('Sales Representative', 70000, 4),
+  ('Service Representative', 60000, 5);
+
+--   Employee Info Table
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Percy', 'Jackson', 1, 1),
+('Harry', 'Potter', 2, 2),
+('Peter', 'Parker', 3, 3),
+('Will', 'Hunting', 4, 4),
+('Frodo', 'Baggins', 5, 5),
+('Logan', 'Howlett', 4, 6);
